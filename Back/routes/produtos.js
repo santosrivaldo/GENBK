895fc624a9b0925518d283f053311caf0f64cd63
Rivaldo <rivaldof66@gmail.com>
2022-08-25 22:12:05 -0300

    envio de imagens

--- Back/routes/produtos.js
+++ Back/routes/produtos.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const mysql = require('../mysql').pool
-
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/'})
 
 
 
@@ -19,7 +20,7 @@ router.get('/', (req, res, next) => {
             (error, result, fields) => {
                 if (error) { return res.status(200).send({ error: error }) }
                 const response = {
-                    quantidade : result.length,
+                    quantidade: result.length,
                     produtos: result.map(prod => {
                         return {
                             id_usuario: prod.id,
@@ -34,7 +35,7 @@ router.get('/', (req, res, next) => {
                     }
                     )
                 }
-                return res.status(200).send({ response})
+                return res.status(200).send({ response })
             }
 
         )
@@ -45,7 +46,8 @@ router.get('/', (req, res, next) => {
 
 });
 
-router.post('/', (req, res, next) => {
+router.post('/',upload.single('login_imagem') , (req, res, next) => {
+    console.log(req.file)
     const produto = {
         id: req.body.id,
         nome: req.body.nome,
@@ -60,7 +62,7 @@ router.post('/', (req, res, next) => {
             (error, result, field) => {
                 conn.release();
                 if (error) { return res.status(500).send({ error: error }) }
-                const response ={
+                const response = {
                     mensagem: 'Produto inserido com sucesso',
                     produtoCriado: {
                         id_usuario: req.body.id,
@@ -71,9 +73,9 @@ router.post('/', (req, res, next) => {
                             descricao: 'Insere um produto',
                             url: 'http://localhost:3200/produtos'
                         }
-                    } 
+                    }
                 }
-                res.status(200).send({response});
+                res.status(200).send({ response });
             }
         )
     })
@@ -89,36 +91,55 @@ router.get('/:id_produto', (req, res, next) => {
             [id],
             (error, result, fields) => {
                 if (error) { return res.status(200).send({ error: error }) }
-                return res.status(200).send({ response: result })
+                if (result.length == 0) {
+                    return res.status(404).send({
+                        mensagem: "Não foi possivel encontrar o produto " + id
+                    })
+                }
+                const response = {
+                    mensagem: 'Produto encontrado',
+                    Produto: {
+                        id_usuario: id,
+                        nome: result.nome,
+                        senha: result.senha,
+                        request: {
+                            tipo: 'GET',
+                            descricao: 'Insere um produto',
+                            url: 'http://localhost:3200/produtos/' + result.id
+                        }
+                    }
+                }
+                return res.status(200).send(response)
             }
-
         )
-    })
+    }
+
+    )
 })
+
 
 
 //router para patch
 
 router.patch('/', (req, res, next) => {
     mysql.getConnection((error, conn) => {
-        if (error) { return res.status(500).send ({ error: error})}
+        if (error) { return res.status(500).send({ error: error }) }
         conn.query(
             `UPDATE usuario
                SET nome  = ?,
                 senha    = ?
-                WHERE id = ?;`, 
-            [   
+                WHERE id = ?;`,
+            [
                 req.body.nome,
                 req.body.senha,
                 req.body.id_usuario
             ],
             (error, resultado, field) => {
                 conn.release();
-                if (error) { return res.status(500).send({ error: error})}
-
+                if (error) { return res.status(500).send({ error: error }) }
                 res.status(202).send({
                     mensagem: 'Produto alterado com sucesso',
-                    
+
                 })
             }
 
@@ -132,22 +153,27 @@ router.patch('/', (req, res, next) => {
 
 router.delete('/', ('/', (req, res, next) => {
     mysql.getConnection((error, conn) => {
-        if (error) { return res.status(500).send ({ error: error})}
+        if (error) { return res.status(500).send({ error: error }) }
         conn.query(
             `DELETE FROM usuario WHERE id = 
             ?`, [req.body.id_usuario],
             (error, resultado, field) => {
                 conn.release();
-                if (error) { return res.status(500).send({ error: error})}
+                if (error) { return res.status(500).send({ error: error }) }
+
+                if (resultado.length == 0) {
+                    return res.status(404).send({})
+                }
 
                 res.status(202).send({
                     mensagem: 'Produto Excluido com sucesso',
-                    
+
                 })
 
             }
 
-    )})
-    }))
+        )
+    })
+}))
 
 module.exports = router;
